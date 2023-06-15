33f2f06e89cc78f280d75ccc0ca04fb236847f75
김명욱 <anko@gsitm.com>
2023-06-15 12:52:23 +0900

    제네릭

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -28,4 +28,59 @@ function logText<T>(text: T): T {
 
 const str = logText<string>('abs');
 str.split('');
-logText<boolean>(true)
+logText<boolean>(true);
+
+
+// interface Dropdown {
+//   value: string;
+//   selected: boolean;
+// }
+
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj: Dropdown<string> = {
+  value: 'abc',
+  selected: false,
+};
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(text => {
+//     console.log(text);
+    
+//   });
+  
+//   return text;
+// }
+// logTextLength<string>(['hi']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+logTextLength('a');
+logTextLength({ length: 10 });
+
+// 제네릭 타입 제한 3 - keyof
+// key 값으로 타입을 지정한다!
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+
+getShoppingItemOption('name');
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,21 @@
-const emails = [
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: DropdownItem<string | number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
