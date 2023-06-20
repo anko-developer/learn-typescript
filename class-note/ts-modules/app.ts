6a8c93ceed8ab999e7aa1b2bb41f1425e7d32e7a
김명욱 <anko@gsitm.com>
2023-06-20 13:00:33 +0900

    clear

---
+++ class-note/ts-modules/app.ts
@@ -0,0 +1,6 @@
+import { Todo } from './types';
+
+let item: Todo = {
+  title: '할 일1',
+  checked: false,
+};
---
+++ class-note/ts-modules/types.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+  title: string;
+  checked: boolean;
+}
+
+// export { Todo };
