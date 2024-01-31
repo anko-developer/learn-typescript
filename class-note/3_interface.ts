d38a5d719a0341dfb582b4ebaff941aa5665e069
김명욱 <anko@gsitm.com>
2024-01-31 14:46:26 +0900

    인터페이스 복습

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -1,85 +1,74 @@
 interface User {
-  name: string;
   age: number;
+  name: string;
+  type?: string;
 }
 
-// 변수에 인터페이스 활용
-const anko: User = {
-  name: 'anko',
-  age: 5
+// 변수에 인터페이스 사용
+let anko: User = {
+  age: 6,
+  name: "anko",
 };
 
-// 함수에 인터페이스 활용
+// 함수에 인터페이스 사용
 function getUser(user: User): void {
   console.log(user);
 }
-const dog = {
-  name: 'anko',
-  age: 100
-}
-getUser(dog);
 
+const capt = {
+  name: "캡틴",
+  age: 30,
+};
 
+getUser(capt);
 
-// 함수의 스펙에 인터페이스를 활용
+// 함수의 스펙에 인터페이스를 사용
 interface SumFunction {
   (a: number, b: number): number;
 }
 
 let sum: SumFunction;
-sum = function(a: number, b: number) {
+sum = (a: number, b: number): number => {
   return a + b;
-}
+};
 
-// 인덱싱 - 인덱스로 접근을해서~
+// 인덱싱
 interface StringArray {
-  // 배열 안에 들어가는 모든 값들이 모두 string 이다
-  [index: number]: string; 
+  // 인덱스 값은 모두 number 값이고,
+  // 그 인덱스에 들어있는 값는 string 값이다.
+  [index: number]: string;
 }
 
-const arr: StringArray = ['a', 'b', 'c'];
-
-// 배열의 인덱싱 방식에 대해서 인터페이스를 string 으로 정의했기 때문에
-// number 가 오면 아래처럼 에러가 난다.
-// arr[0] = 10; 
-
+const arr = ["a", "b", "c"];
+arr[0]; // "a"
 
 // 딕셔너리 패턴
 interface StringRegexDictionary {
-  // 왼쪽 키값은 string, 오른쪽 속성 값은 정규식으로
   [key: string]: RegExp;
 }
 
 const obj: StringRegexDictionary = {
-  sth: /abc/, 
+  // sth: /abc/,
   cssFile: /\.css$/,
-  jsfile: /\.js$/,
-  // cssFile: 'css',
+  jsFuke: /\.js$/,
 };
+// obj["cssFile"] = "a"; // error, 내가 정한 interface 규칙에 어긋났기 때문
 
-// interface에서는 정규식이 정의되어야하는데 문자열을 넣었으니 에러남
-// obj['cssFile'] = 'a';
+Object.keys(obj).forEach(function (value) {});
 
-Object.keys(obj).forEach((value) => {
-  console.log(value);
-});
-
-
-// 인터페이스 확장(상속)
+// 인터페이스 확장
 interface Person {
   name: string;
   age: number;
+  job?: string;
 }
 
-// Person 의 interface 를 상속 받아서 사용할 수 있음
-interface Developer extends Person {
-  // name: string;
-  // age: number;
-  lang: string;
+interface Developter extends Person {
+  language: string;
 }
 
-const wook: Developer = {
-  lang: 'ts',
-  age: 100,
-  name: '욱'
+const wook: Developter = {
+  language: "ts",
+  name: "명욱",
+  age: 37,
 };
