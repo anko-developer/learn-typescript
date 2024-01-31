a2e141ef094ebba7b694797e0a07ff44648604ac
김명욱 <anko@gsitm.com>
2024-01-31 17:20:07 +0900

    이넘 숫자 타입

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,25 +1,7 @@
 enum Shoes {
-  Nike = '나이키',
-  Adida = '아디다스',
+  Nike,
+  Adidas,
 }
 
-const myShoes = Shoes.Nike;
-console.log(myShoes); // '나이키'
-
-// 예제
-
-enum Answer {
-  Yes = 'Y',
-  No = 'N',
-}
-
-function askQuestion(answer: Answer) {
-  if (answer === Answer.Yes) {
-    console.log('정답');
-  }
-  if (answer === Answer.No ) {
-    console.log('오답');
-  }
-}
-askQuestion(Answer.Yes);
-askQuestion('Yes');
+let myShoes = Shoes.Nike;
+console.log(myShoes);
