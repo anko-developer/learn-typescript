9e802eb3e71a4b29f0ece5e8756516623097151a
김명욱 <anko@gsitm.com>
2024-02-02 15:59:42 +0900

    제네릭 타입 제한

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -24,3 +24,38 @@ console.log(replace);
 
 const login = logText<boolean>(true);
 console.log(login);
+
+// 제네릭 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//   text.forEach((text) => {
+//     console.log(text);
+//   });
+//   return text;
+// }
+
+// logTextLength<string>(["hi", "bye"]);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+logTextLength<string>("abc");
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+
+// getShoppingItemOption(10);
+getShoppingItemOption("name");
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -4,9 +4,9 @@ interface DropdownItem<T> {
 }
 
 const emails: DropdownItem<string>[] = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
 ];
 
 const numberOfProducts: DropdownItem<number>[] = [
@@ -15,8 +15,9 @@ const numberOfProducts: DropdownItem<number>[] = [
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: DropdownItem<string | number>) {
-  const option = document.createElement('option');
+// toString이라는 함수 메서드를 가진 타입만 제네릭으로 받는다고 제네릭 타입제한을 명시할 수 있다
+function createDropdownItem<T>(item: DropdownItem<T extends { toString: Function }>) {
+  const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
   option.selected = item.selected;
@@ -25,7 +26,7 @@ function createDropdownItem(item: DropdownItem<string | number>) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
