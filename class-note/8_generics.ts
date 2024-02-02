7bd02bba2cabc2b9e036be21750418416ea836e5
김명욱 <anko@gsitm.com>
2024-02-02 11:06:57 +0900

    제네릭 복습

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,4 +1,4 @@
-// function logText(text) {
+// function logText(text: string) {
 //   console.log(text);
 //   return text;
 // }
@@ -7,80 +7,20 @@
 //   console.log(text);
 //   return text;
 // }
-// logText<string>('하이');
 
-// function logText(text: string) {
-//   console.log(text);
-//   // text.split('').reverse().join('');
-//   return text;
-// }
+// logText<string>("하이");
 
-// function logNumber(num: number) {
-//   console.log(num);
-//   return num;
-  
-// }
-
+// 실제로 함수를 정의할 때 타입을 비어놓은 상태에서
+// 그 타입에 뭐가 들어 갈 거다를 호출한 시점에 정의하는 것이 바로 제네릭
+// 타입을 추론해서 최종 반환값 까지 붙일 수 있는 것이 바로 제네릭!
 function logText<T>(text: T): T {
   console.log(text);
   return text;
 }
 
-const str = logText<string>('abs');
-str.split('');
-logText<boolean>(true);
+const str = logText<string>("abcdefg");
+const replace = str.split("").reverse().join("");
+console.log(replace);
 
-
-// interface Dropdown {
-//   value: string;
-//   selected: boolean;
-// }
-
-interface Dropdown<T> {
-  value: T;
-  selected: boolean;
-}
-
-const obj: Dropdown<string> = {
-  value: 'abc',
-  selected: false,
-};
-
-// 제네릭의 타입 제한
-// function logTextLength<T>(text: T[]): T[] {
-//   console.log(text.length);
-//   text.forEach(text => {
-//     console.log(text);
-    
-//   });
-  
-//   return text;
-// }
-// logTextLength<string>(['hi']);
-
-// 제네릭 타입 제한 2 - 정의된 타입 이용하기
-interface LengthType {
-  length: number;
-}
-function logTextLength<T extends LengthType>(text: T): T {
-  text.length;
-  return text;
-}
-logTextLength('a');
-logTextLength({ length: 10 });
-
-// 제네릭 타입 제한 3 - keyof
-// key 값으로 타입을 지정한다!
-interface ShoppingItem {
-  name: string;
-  price: number;
-  stock: number;
-}
-
-function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
-  return itemOption;
-}
-
-getShoppingItemOption('name');
-// getShoppingItemOption(10);
-// getShoppingItemOption<string>('a');
+const login = logText<boolean>(true);
+console.log(login);
