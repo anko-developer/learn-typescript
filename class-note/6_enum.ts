ae8c49f9efbbc11456bc77db1190696e82538d85
김명욱 <anko@gsitm.com>
2024-02-02 09:56:01 +0900

    이넘 활용 예제

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -4,4 +4,20 @@ enum Shoes {
 }
 
 let myShoes = Shoes.Nike;
-console.log(myShoes);
+console.log(myShoes); // '나이키'
+
+// 예제
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+function askQuestion(answer: Answer): void {
+  if (answer === Answer.Yes) {
+    console.log("정답", Answer.Yes);
+  }
+  if (answer === Answer.No) {
+    console.log("오답", Answer.No);
+  }
+}
+askQuestion(Answer.Yes);
